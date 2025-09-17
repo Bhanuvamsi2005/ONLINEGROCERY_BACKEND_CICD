2b0c96fcaffc5cd8e2a1a256e1d0cfcb3d1cdea5
Bhanuvamsi2005 <bhanuvamsia@gmail.com>
2025-09-17 19:48:20 +0530

    Update KhatabackendApplication.java

--- src/main/java/com/example/khatabackend/KhatabackendApplication.java
+++ src/main/java/com/example/khatabackend/KhatabackendApplication.java
@@ -1,15 +1,28 @@
+//package com.example.khatabackend;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.context.properties.EnableConfigurationProperties;
+// import com.example.khatabackend.auth.TwilioConfig;
+// import com.twilio.Twilio;
+// import jakarta.annotation.PostConstruct;
+
+//@SpringBootApplication
+// @EnableConfigurationProperties(TwilioConfig.class) // Tell Spring to bind this config
+// public class KhatabackendApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(KhatabackendApplication.class, args);
+//     }
+// }
 package com.example.khatabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.boot.context.properties.EnableConfigurationProperties;
-// import com.example.khatabackend.auth.TwilioConfig;
-import com.twilio.Twilio;
-import jakarta.annotation.PostConstruct;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-// @EnableConfigurationProperties(TwilioConfig.class) // Tell Spring to bind this config
-public class KhatabackendApplication {
+public class KhatabackendApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(KhatabackendApplication.class, args);
