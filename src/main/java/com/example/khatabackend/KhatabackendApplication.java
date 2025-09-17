a99c6d3c2dfe0479580305604e09009bfaa11726
Bhanuvamsi2005 <bhanuvamsia@gmail.com>
2025-09-17 19:56:30 +0530

    Update KhatabackendApplication.java

--- src/main/java/com/example/khatabackend/KhatabackendApplication.java
+++ src/main/java/com/example/khatabackend/KhatabackendApplication.java
@@ -19,10 +19,16 @@ package com.example.khatabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class KhatabackendApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(KhatabackendApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KhatabackendApplication.class, args);
